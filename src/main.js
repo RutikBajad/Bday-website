e9fa51ede279d8bbf57bfb9e5b090e154f64b0d7
Rutik Bajad <rutikbajad9699@gmail.com>
2026-09-11 00:01:32 +0530

    Schedule birthday event for 12:10 AM

--- src/main.js
+++ src/main.js
@@ -70,7 +70,7 @@ const starsContainer = document.querySelector(".stars");
 const demoBirthdayNow = false;
 const birthdayEventTime = demoBirthdayNow
   ? new Date()
-  : new Date(2026, 6, 25, 0, 0, 0);
+  : new Date(2026, 6, 25, 0, 10, 0);
 const birthdayEventWindowMs = 5 * 60 * 1000;
 
 function createShootingStar() {
